ca3e9ee10bc988a4a746864740d1d92581d2d75c
PAT <ptman@mail.ru>
2023-11-26 14:51:34 +0300

    cookie-clicker changed

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,7 +2,17 @@ const img = document.getElementById("cookie")
 const counter = document.getElementById("clicker__counter")
 
 img.onclick = () => {
-    console.log(counter.textContent)
+
     counter.textContent = Number(counter.textContent) + 1
+
+    if (Number(counter.textContent)%2) {
+
+    img.width = img.width * 2
+    img.height = img.height * 2
+    }
+    else {
+        img.width = img.width / 2
+        img.height = img.height / 2
+    }
 
 }
